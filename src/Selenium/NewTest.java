9e2749e3a090e2a14a7f9f284a6a014518437133
HP <HP@DESKTOP-KQP1K4Q>
2021-06-30 16:06:58 +0530

    commit

--- src/Selenium/NewTest.java
+++ src/Selenium/NewTest.java
@@ -1,13 +1,18 @@
 package Selenium;
-
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeTest;
+
+import java.io.IOException;
+import java.net.URL;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterTest;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class NewTest {
 	  public WebDriver driver;
@@ -16,14 +21,12 @@ public class NewTest {
 		  public void f() {  
 			  driver.get("https://aws.amazon.com/console/");
 		  }
-
-	  
 		 	 @BeforeTest
-	  public void beforeTest() {
+	  public void beforeTest() throws IOException {
 		  System.setProperty("webdriver.gecko.driver","/usr/bin/geckodriver");
 		  driver= new FirefoxDriver();	  
-	  }
-	  
+		 	 }
+
 	  @AfterTest
 	  public void afterTest() { 
 		  driver.close();
